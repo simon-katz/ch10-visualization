49a77ef39cf5c6f720c7f5e05b8f6c594a4f4894
Simon Katz <sk@nomistech.com>
2020-01-06 19:05:05 +0000

    Upgrade to Clojure 1.10.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/clojuredatascience/ch10-visualization"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "0.0-3126"]
                  [org.clojure/tools.cli "0.3.1"]
                  [quil "2.2.5" :exclusions [org.clojure/clojure]]
